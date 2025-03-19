4d0efc318ca69cd9ab045152a6e02300bdf549fd
jalalk2012 <jalalk2012@gmail.com>
2025-03-19 14:41:40 +0600

    add blog module

---
+++ app/Http/Controllers/BlogBlogsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\blogBlogs;
+use Illuminate\Http\Request;
+
+class BlogBlogsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(blogBlogs $blogBlogs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(blogBlogs $blogBlogs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, blogBlogs $blogBlogs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(blogBlogs $blogBlogs)
+    {
+        //
+    }
+}
